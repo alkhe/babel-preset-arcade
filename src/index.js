9d282fb547ddccd3433d742ce0b5590e1b495aac
edge <functaire@gmail.com>
2016-11-10 12:21:34 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import syntax_plugin from 'babel-plugin-syntax-jsx'
+import transform_plugin from 'babel-plugin-transform-arcade-jsx'
+
+export default {
+	plugins: [syntax_plugin, transform_plugin]
+}
